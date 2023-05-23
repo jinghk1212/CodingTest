afb373911fafe90df7f1ec6fa1c1f3894a57df05
jinghk1212 <jinghk1212@naver.com>
2023-05-24 02:16:36 +0900

    Java Convention

---
+++ CodingPrac/src/may/Day241st.java
@@ -0,0 +1,17 @@
+package may;
+
+public class Day241st {
+	
+	// Java Convention
+	/* 
+	 * 1. 패키지 이름은 모두 소문자로 작성. 도메인 이름을 거꾸로 사용하여 지정. (com.example.project)
+	 * 2. 클래스 이름은 대문자로 시작. Camel 표기법 사용. (MyNewProject)
+	 * 3. 메소드, 변수 이름은 소문자로 시작. Camel 표기법 사용. (calculateTotal)
+	 * 4. 상수 이름은 모두 대문자. final로 작성 (MAX_VALUE_EXAMPLE)
+	 * 5. Tab = 4칸 (고정!), 가독성을 위해 공백과 들여쓰기 사용.
+	 * 6. 멤버변수는 private, getter setter제공. (필요시)
+	 * 7. 예외는 try-catch.
+	 * 8. 패키지 구조 (가독성, 일관성, 유지보수성) ex)MVC패턴?
+	 */
+
+}

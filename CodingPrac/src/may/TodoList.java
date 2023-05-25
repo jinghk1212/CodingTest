328527c128bba8b9fbcfb33d0080d04720e6ad69
jinghk1212 <jinghk1212@naver.com>
2023-05-25 23:23:40 +0900

    TODO LIST.

---
+++ CodingPrac/src/may/TodoList.java
@@ -0,0 +1,15 @@
+package may;
+
+public class TodoList {
+	// Todo List
+	/*
+	 1. Java 기본: 클래스, 객체, 상속, 인터페이스, 예외 다시 공부.
+	 2. 웹 기술: JavaScript(프론트 외), 웹 동작원리, HTTP 등 공부
+	 3. Servlet: Servlet 동작원리, 생명주기, 요청 처리 및 응답에 필요한 메서드
+	 4. JSP: 기본문법, 지시자, EL 등.
+	 5. Spring, Spring Boot: 차이점 인지, 공부.
+	 6. DB: ANSI방식 SQL문, Join 3가지 방식 공부.
+	 7. 테스트: 유닛테스트 방식 확인.
+	 8. TODO 추가 예정
+	 */
+}

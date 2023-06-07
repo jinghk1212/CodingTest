c315c7f0c59b5b6ff9d6d8396565c90386c453c6
jinghk1212 <jinghk1212@naver.com>
2023-06-07 23:05:43 +0900

    Spring vs SpringBoot

---
+++ CodingPrac/src/june/Day71st.java
@@ -0,0 +1,13 @@
+package june;
+
+public class Day71st {
+ // Spring과 SpringBoot의 차이점
+ /*
+  1. 설정의 간소화. Spring은 xml기반의 설정, SpringBoot는 xml이 없어도 가능.
+  2. SpringBoot에는 내장 서버가 있다. Spring은 반드시 외부 서버에 애플리케이션을 배포.
+  3. 의존성의 편의성. Spring에서는 개별적으로 관리.
+  4. SpringBoot는 실행 환경에 따라 자동으로 설정을 조정.
+  5. SpringBoot는 기본 설정들이 많아 간편하게 세팅할 수 있다.
+  ...
+  */
+}
